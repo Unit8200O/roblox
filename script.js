ee5986b669942dc60d1f166344d0b177ae800cde
Unit8200O <isr43ls3cr3t@proton.me>
2024-12-31 17:09:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ document.getElementsByTagName("html")[0].innerHTML = `
             position: absolute;
             left: 0;
             width: 100%;
-            height: 30%;
+            height: 30px;
             background: #0038b8;
         }
 
@@ -103,6 +103,15 @@ document.getElementsByTagName("html")[0].innerHTML = `
 
         .israel-flag::after {
             bottom: 0;
+        }
+
+        .star-of-david {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 60px;
+            height: 60px;
         }
 
         .star-of-david {
